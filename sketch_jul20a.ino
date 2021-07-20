f944a2555701f14a4035346fd91f2efdd74c2c13
Riya0603 <64098822+Riya0603@users.noreply.github.com>
2021-07-20 14:59:25 +0600

    Add files via upload

---
+++ sketch_jul20a.ino
@@ -0,0 +1,35 @@
+
+
+LiquidCrystal_I2C lcd(0x27, 16,2);
+const int triggerPin = 3;
+const int echoPin = 2;
+long duration;
+int distanceCm, distanceInch;
+void setup() {
+  lcd.init();
+  lcd.begin(16, 2); // Initializes the interface to the LCD display
+  lcd.backlight();
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(15);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distanceCm = duration * 0.0340 / 2;
+  distanceInch = duration * 0.01330 / 2;
+  lcd.setCursor(0, 0);
+  lcd.print("Distance: ");
+  Serial.println(distanceCm);
+  lcd.print(distanceCm);
+  lcd.print(" cm");
+  lcd.setCursor(0, 1);
+  lcd.print("    ");
+  lcd.print(distanceInch);
+  lcd.print(" inch");
+  delay(500);
+}
